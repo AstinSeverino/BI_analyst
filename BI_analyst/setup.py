1fb15663e39d0ea9d89bdf2a87766193e544bb92
astin <astin464@gmail.com>
2023-04-29 18:00:29 -0400

    El primer commit

---
+++ BI_analyst/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analisis explorio sobre un embudo de ventas de tarjestas de credito',
+    author='Astin Severino',
+    license='MIT',
+)
